d4e79f67d9ea7d489f1f6b5153ec81fe6a248b3b
njogued <njogued@gmail.com>
2025-10-29 14:50:18 +0300

    form registrations

---
+++ formRegAppsScript.js
@@ -0,0 +1,31 @@
+function onFormSubmit(e) {
+  const nv = e.namedValues;
+  let formResponse = e.response;
+  let itemResponses = formResponse.getItemResponses();
+  let fname, emailAddress;
+  for (var i = 0; i < itemResponses.length; i++) {
+    var title = itemResponses[i].getItem().getTitle();
+    var response = itemResponses[i].getResponse();
+    switch (title) {
+      case "Name":
+        fname = response.split(" ")[0];
+        break;
+      case "Email address":
+        emailAddress = response;
+        break;
+    }
+  }
+  // let fname = 'Edward';
+  // let emailAddress = "njogued@gmail.com";
+  const subject = "You’re confirmed";
+  const htmlBody = `<p style="margin:0 0 16px;">Hi ${fname},</p>
+  <p style="margin:0 0 16px;">Thank you for registering for the session!</p>
+    //   more content goes here...
+  <p style="margin:0 0 16px;">Best regards,<br/>Your Name</p>`;
+  GmailApp.sendEmail(
+    emailAddress,
+    subject,
+    "// This is a fallback plain text body (Optional)",
+    { htmlBody, name: "Your Name" }
+  );
+}
